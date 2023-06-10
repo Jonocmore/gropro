815afcc3e5ca4edbadc9536c23ee9cebc36b467a
Percy Manenzhe <percy@ptpi.co.za>
2023-06-10 16:06:15 +0200

    updated_type_to_file_type

---
+++ db/migrate/20230610140459_rename_type_to_file_type_in_resources.rb
@@ -0,0 +1,4 @@
+class RenameTypeToFileTypeInResources < ActiveRecord::Migration[7.0]
+  def change
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_06_10_112319) do
+ActiveRecord::Schema[7.0].define(version: 2023_06_10_140459) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -89,7 +89,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_06_10_112319) do
     t.string "text"
     t.string "author"
     t.string "date"
-    t.string "type"
+    t.string "file_type"
     t.string "url"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
