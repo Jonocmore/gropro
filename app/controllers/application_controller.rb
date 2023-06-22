6eb051ad65f0b2d6a51d48d12f6b3f64d6725495
Percy Manenzhe <percy@ptpi.co.za>
2023-06-22 20:13:36 +0200

    made_some_changes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,7 +7,7 @@ class ApplicationController < ActionController::Base
 
   def after_sign_in_path_for(resource)
     if resource.is_a?(User)
-      user_gardens_path(resource)
+      user_session_path(resource)
     else
       super
     end
