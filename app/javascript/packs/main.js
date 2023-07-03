76c4b2b8a4945d4be07b88f5feba2c070fd62cc3
Hendrik Oosthuizen <hendrik@thecodeblock.net>
2023-07-03 21:17:25 +0200

    add weather to garden page

---
+++ app/javascript/packs/main.js
@@ -0,0 +1,64 @@
+import { getWeather } from "./weather"
+import { ICON_MAP } from "./iconMap"
+
+navigator.geolocation.getCurrentPosition(positionSuccess, positionError)
+
+function positionSuccess({ coords }) {
+  getWeather(
+    coords.latitude,
+    coords.longitude,
+    Intl.DateTimeFormat().resolvedOptions().timeZone
+  )
+    .then(renderWeather)
+    .catch(e => {
+      console.error(e)
+      alert("Error getting weather.")
+    })
+}
+
+function positionError() {
+  alert(
+    "There was an error getting your location. Please allow us to use your location and refresh the page."
+  )
+}
+
+function renderWeather({ current, daily }) {
+  renderCurrentWeather(current)
+  renderDailyWeather(daily)
+  // document.body.classList.remove("blurred")
+}
+
+function setValue(selector, value, { parent = document } = {}) {
+  parent.querySelector(`[data-${selector}]`).textContent = value
+}
+
+function getIconUrl(iconCode) {
+  return `icons/${ICON_MAP.get(iconCode)}.svg`
+}
+
+const currentIcon = document.querySelector("[data-current-icon]")
+console.log(currentIcon)
+function renderCurrentWeather(current) {
+  // currentIcon.src = getIconUrl(current.iconCode)
+  setValue("current-temp", current.currentTemp)
+  setValue("current-high", current.highTemp)
+  setValue("current-low", current.lowTemp)
+  setValue("current-fl-high", current.highFeelsLike)
+  setValue("current-fl-low", current.lowFeelsLike)
+  setValue("current-wind", current.windSpeed)
+  setValue("current-precip", current.precip)
+}
+
+const DAY_FORMATTER = new Intl.DateTimeFormat(undefined, { weekday: "long" })
+const dailySection = document.querySelector("[data-day-section]")
+const dayCardTemplate = document.getElementById("day-card-template")
+function renderDailyWeather(daily) {
+  dailySection.innerHTML = ""
+  daily.forEach(day => {
+    const element = dayCardTemplate.content.cloneNode(true)
+    setValue("temp", day.maxTemp, { parent: element })
+    setValue("date", DAY_FORMATTER.format(day.timestamp), { parent: element })
+    element.querySelector("[data-icon]").src = getIconUrl(day.iconCode)
+    dailySection.append(element)
+  })
+}
