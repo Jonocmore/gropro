dffa8e09268b8bae41042dfc44e22e4a8494236e
Hendrik Oosthuizen <hendrik@thecodeblock.net>
2023-07-03 11:11:43 +0200

    add search filter to resource page

--- app/controllers/resources_controller.rb
+++ app/controllers/resources_controller.rb
@@ -1,9 +1,25 @@
 class ResourcesController < ApplicationController
   skip_before_action :authenticate_user!, only: :index
+
   def index
     @resources = Resource.all
+
+    # Search
+
+    popular = params[:popular]
+
+    if params[:query].present?
+      @resources = Resource.search_by_title(params[:query])
+    else
+      @resources = if popular.present?
+        Resource.where(popular: true)
+      else
+        Resource.order(:title)
+    end
   end
+end
 
   def show
   end
+
 end
