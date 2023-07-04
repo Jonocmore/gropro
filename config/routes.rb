dca8c20e9077befbd3216e9c4e95b032119500c7
Jono More <jono@imaliquiz.com>
2023-07-04 20:59:10 +0200

    unsaved file

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,11 @@ Rails.application.routes.draw do
 
   devise_for :users
 
-  resources :gardens, except: [:destroy] do
+  resources :gardens do
+    member do
+      post "add_plant" # Route for adding a plant to a garden
+      delete "remove_plant_from_garden" # Route for removing a plant from a garden
+    end
     resources :recommendations, only: %i[index create show]
     resources :plants, only: %i[index show destroy]
   end
